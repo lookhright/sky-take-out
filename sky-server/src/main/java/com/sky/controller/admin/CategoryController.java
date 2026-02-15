4e249fe625684c973eec018dbafb263f6b929f9c
Tigger <lookhright@gmail.com>
2026-02-15 23:01:13 +0800

    remove secret

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -6,6 +6,7 @@ import com.sky.entity.Category;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
+import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -97,9 +98,9 @@ public class CategoryController {
     @GetMapping("/list")
     @ApiOperation("根据类型查询分类")
     public Result<List<Category>> list(Integer type){
-
-        System.out.println("type = " + type);
         List<Category> list = categoryService.list(type);
         return Result.success(list);
     }
+
+
 }
--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,14 +2,16 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -24,10 +26,36 @@ public class DishController {
      * @param dishDTO
      * @return
      */
-    public Result save(@RequestBody DishDTO dishDTO){
+    @PutMapping
+    public Result save (@RequestBody DishDTO dishDTO){
         log.info("新增菜品：{}",dishDTO);
 
         return Result.success();
     }
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<PageResult> page (DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品分页查询：{}",dishPageQueryDTO);
+       PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return  Result.success(pageResult);
 
+    }
+    /**
+     * 菜品批量删除
+     * @param ids
+     * @return
+     */
+
+    @DeleteMapping
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("菜品批量删除：{}",ids);
+        dishService.deleteBatch(ids);
+
+        return Result.success();
+
+    }
 }
